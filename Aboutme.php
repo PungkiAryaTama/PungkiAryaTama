022a1ff4f99476ea32c9a18977033aef2f78d2c9
PungkiAryaTama <63331154+PungkiAryaTama@users.noreply.github.com>
2020-04-08 13:13:14 +0700

    Add files via upload

---
+++ Aboutme.php
@@ -0,0 +1,8 @@
+<?php
+echo "Nama    : Pungki Arya Tama";
+echo "<br>";
+echo "Kelas   : XI RPL 2";
+echo "<br>";
+echo "Sekolah : SMK  Taruna Bhakti Depok";
+echo "<br>";
+echo "Alamat  : Jln. Tole Iskandar, GG.H.Inan RT 07/ RW 04 No.47 Kp.Cipayung-Sukmajaya Depok";
