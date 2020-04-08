e651779d111da8d9372a189d10162463a79a76ed
PungkiAryaTama <63331154+PungkiAryaTama@users.noreply.github.com>
2020-04-08 13:29:37 +0700

    Update Aboutme.php

--- Aboutme.php
+++ Aboutme.php
@@ -1,10 +1,12 @@
 <?php
 echo "<h1> About Me </h1>";
 
-echo "Nama    : Pungki Arya Tama";
+echo "Nama     : Pungki Arya Tama";
 echo "<br>";
-echo "Kelas   : XI RPL 2";
+echo "Kelas    : XI RPL 2";
 echo "<br>";
-echo "Sekolah : SMK  Taruna Bhakti Depok";
+echo "Sekolah  : SMK  Taruna Bhakti Depok";
 echo "<br>";
-echo "Alamat  : Jln. Tole Iskandar, GG.H.Inan RT 07/ RW 04 No.47 Kp.Cipayung-Sukmajaya Depok";
+echo "Nomor Hp : 081285588848";
+echo "<br>";
+echo "Email    : aryatamapungki@gmail.com";
