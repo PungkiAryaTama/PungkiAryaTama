b2b862324f81eeadd143f41d6c69539907baaf8d
PungkiAryaTama <63331154+PungkiAryaTama@users.noreply.github.com>
2020-06-13 10:10:52 +0700

    Update MyToken.py

--- MyToken.py
+++ MyToken.py
@@ -1,2 +1,4 @@
 TOKEN ='1207918586:AAEmO8A6D4h1kDIABvLWbOgI58zo5Ww49PQ'
-Reply= "Pungki Arya Tama"
+Reply= "Hallo, Ini Adalah Bot Pertamaku Untuk Tugas PWPB SMK Taruna Bhakti"
+Reply1= " ~~ Data Siswa Kelas XI Rekayasa Perangkat Lunak ✔ ~~ "
+Help = " ~~ Hallo Apa Yang Bisa Aku Bantu? 😊 ~~"
